74c329de14b00721054d641506611e6e50cafa6d
fetorres <fetorres@groupon.com>
2016-07-04 13:00:09 -0300

    finishing templating

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 module.exports = {
   entry: './js/entry.js',
   output: {
     path: __dirname,
     filename: 'bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      filename: 'index.html',
+      template: `${__dirname}/template/index.hbs`,
+    }),
+  ],
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style!css' },
@@ -15,6 +24,14 @@ module.exports = {
         loader: 'babel', // 'babel-loader' is also a legal name to reference
         query: {
           presets: ['node6'],
+        },
+      },
+      { test: /\.(handlebars|hbs)$/, loader: 'handlebars-loader',
+        query: {
+          partialDirs: [
+            path.join(__dirname, 'template', 'partials'),
+            path.join(__dirname, 'template'),
+          ],
         },
       },
     ],
